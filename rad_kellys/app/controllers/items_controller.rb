37d3212db08d5a93d4583fb5e3eda5091c382cfc
David Johnson <david_jono@hotmail.com>
2020-03-06 16:20:07 +1000

    main controllers routes views

--- rad_kellys/app/controllers/items_controller.rb
+++ rad_kellys/app/controllers/items_controller.rb
@@ -4,5 +4,62 @@ class ItemsController < ApplicationController
         @items = Item.all
 
     end
+   
+
+    def new
+        @items = Item.new
+    end
+
+
+    def all
+        @items = Item.all 
+    end
+
+    def create
+      
+        @items = Item.new(items_params)
+
+        if @items.valid? && @items.save
+        #if its a post you do a redirect if its a view you do a render
+        redirect_to items_path
+        else
+        render :new
+
+        end
+    end 
+
+    def show
+        @items = Item.find(params[:id])
+    end
+
+    def edit
+        @items = Item.find(params[:id])
+    
+    end
+
+    def update
+        @items = Item.find(params[:id])
+
+        @items.update!(items_params)
+        if @items.valid? && @items.save
+            redirect_to items_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        Item.destroy(params[:id])
+        redirect_to items_path
+    end
+
+    
+    #anything bleow this is private and the user will not see it
+    private
+    def items_params
+    params.require(:items).permit(:name, :category, :colour, :texture, :days_to_cultivate, :price, :user_id)
+    end
+
+
 
 end
--- rad_kellys/config/routes.rb
+++ rad_kellys/config/routes.rb
@@ -3,13 +3,41 @@ Rails.application.routes.draw do
     registrations: 'user/registrations'
   }
  
-  root to: "pages#index"
+    root to: "pages#index"
 
-  get '/', to: 'pages#index', as: 'homepage'
-  get '/about', to: 'pages#about', as: 'about'
-  get '/option', to: 'pages#option', as: 'option'
+    get '/', to: 'pages#index', as: 'homepage'
+    get '/about', to: 'pages#about', as: 'about'
+    get '/option', to: 'pages#option', as: 'option'
   
-  get '/items', to: 'items#index', as: 'items'
+    #show list
+    get '/items', to: 'items#index', as: 'items'
+    #always put an ailas as: on a get method
+
+    #show all items for sale
+    get '/items/all', to: 'items#all', as: 'all_items'
+ 
+    #Create items
+    get '/items/new', to: 'items#new', as: 'new_item'
+    post '/items', to: 'items#create'
+  
+ 
+    # #Read items
+    get '/items/:id', to: 'items#show', as: 'item'
+ 
+    #Edit items
+    get '/items/:id/edit', to: 'items#edit', as: 'edit_item'
+   
+   
+    #Update items
+    patch '/items/:id', to: 'items#update'
+    put '/items/:id', to: 'items#update'
+ 
+    #Delete items
+    delete '/items/:id', to: 'items#destroy', as: 'destroy_item'
+
+
+
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
